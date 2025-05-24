2be07ae8a61762406f4ce5fb60c92e9f038a3bdd
Elavarasan.t <95843820+Elavarasan69@users.noreply.github.com>
2025-05-24 13:15:26 +0530

    Admin add and list

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,18 +5,24 @@ import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+
+  const BASE_URL = "http://localhost:4000"
+
   return (
     <div>
+      <ToastContainer/>
       <Navbar/>
       <hr />
       <div className="app-content">
         <Sidebar/>
         <Routes>
-          <Route path='/add' element={<Add/>}/>
-          <Route path='/list' element={<List/>}/>
-          <Route path='/orders' element={<Orders/>}/>
+          <Route path='/add' element={<Add url={BASE_URL}/>}/>
+          <Route path='/list' element={<List url={BASE_URL}/>}/>
+          <Route path='/orders' element={<Orders url={BASE_URL}/>}/>
         </Routes>
       </div>
     </div>
